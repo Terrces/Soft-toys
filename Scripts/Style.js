b02e69fbe06f6648bf00d0b1a1451af07bcd9c68
terrces <113709421+Terrces@users.noreply.github.com>
2024-10-26 17:00:22 +0500

    Mobile Test

--- Scripts/Style.js
+++ Scripts/Style.js
@@ -1,5 +1,7 @@
 if(document.body.opacity = "0%")
 setTimeout(() => {document.body.style.opacity = "100%";},1)
+let BackgroundAnimationSpeed = 100;
 let BackgroundPos = 0;
-setInterval(() => {BackgroundPos+=10},100)
-setInterval(() => {document.body.style.backgroundPosition = BackgroundPos+"px" },100)
+setInterval(() => {BackgroundPos+=10},BackgroundAnimationSpeed)
+setInterval(() => {document.body.style.backgroundPositionY = BackgroundPos+"px" },BackgroundAnimationSpeed)
+setInterval(() => {document.body.style.backgroundPositionX = BackgroundPos+"px" },BackgroundAnimationSpeed)
