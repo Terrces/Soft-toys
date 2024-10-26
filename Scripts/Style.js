36bd718f3fc20027ef9609b99ae092ba815b386a
terrces <113709421+Terrces@users.noreply.github.com>
2024-10-26 16:31:59 +0500

    New background

--- Scripts/Style.js
+++ Scripts/Style.js
@@ -1,2 +1,5 @@
 if(document.body.opacity = "0%")
 setTimeout(() => {document.body.style.opacity = "100%";},1)
+let BackgroundPos = 0;
+setInterval(() => {BackgroundPos+=10},100)
+setInterval(() => {document.body.style.backgroundPosition = BackgroundPos+"px" },100)
