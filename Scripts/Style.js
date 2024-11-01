c8a113ca9db0019d6f3efc4cb07eb857429d6a5b
terrces <113709421+Terrces@users.noreply.github.com>
2024-11-01 14:35:36 +0500

    mini upd

--- Scripts/Style.js
+++ Scripts/Style.js
@@ -1,7 +1,10 @@
 if(document.body.opacity = "0%")
 setTimeout(() => {document.body.style.opacity = "100%";},1)
-let BackgroundAnimationSpeed = 100;
+let BackgroundAnimationSpeed = 15000;
 let BackgroundPos = 0;
-setInterval(() => {BackgroundPos+=10},BackgroundAnimationSpeed)
+BackgroundPos+=1920
+document.body.style.backgroundPositionX = BackgroundPos+"px"
+document.body.style.backgroundPositionY = BackgroundPos+"px"
+setInterval(() => {BackgroundPos+=1920},BackgroundAnimationSpeed)
 setInterval(() => {document.body.style.backgroundPositionY = BackgroundPos+"px" },BackgroundAnimationSpeed)
 setInterval(() => {document.body.style.backgroundPositionX = BackgroundPos+"px" },BackgroundAnimationSpeed)
