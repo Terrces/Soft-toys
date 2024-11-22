0dfffaadf95ebb0d452c46e9cf5a73ae4d494f0e
terrces <terrceszero@gmail.com>
2024-11-22 14:31:30 +0500

    Сегодня бебры

--- Scripts/Style.js
+++ Scripts/Style.js
@@ -2,23 +2,10 @@ if(document.body.opacity = "0%")
 setTimeout(() => {document.body.style.opacity = "100%";},1)
 let BackgroundAnimationSpeed = 0;
 let BackgroundPos = 0;
-document.addEventListener("DOMContentLoaded", () => 
-{
-    BackgroundAnimationSpeed = 15000;
-    BackgroundPos+=1920;
-    document.body.style.backgroundPositionX = BackgroundPos+"px"
-    document.body.style.backgroundPositionY = BackgroundPos+"px"
-    setInterval(() => {BackgroundPos+=1920},BackgroundAnimationSpeed)
-    setInterval(() => {document.body.style.backgroundPositionY = BackgroundPos+"px" },BackgroundAnimationSpeed)
-    setInterval(() => {document.body.style.backgroundPositionX = BackgroundPos+"px" },BackgroundAnimationSpeed)
-})
-document.addEventListener("beforeunload", () =>
-{
-    BackgroundAnimationSpeed = 0;
-    BackgroundPos=0;
-    document.body.style.backgroundPositionX = BackgroundPos+"px"
-    document.body.style.backgroundPositionY = BackgroundPos+"px"
-    setInterval(() => {BackgroundPos+=0},BackgroundAnimationSpeed)
-    setInterval(() => {document.body.style.backgroundPositionY = BackgroundPos+"px" },BackgroundAnimationSpeed)
-    setInterval(() => {document.body.style.backgroundPositionX = BackgroundPos+"px" },BackgroundAnimationSpeed)    
-})
+BackgroundAnimationSpeed = 15000;
+BackgroundPos+=1920;
+document.body.style.backgroundPositionX = BackgroundPos+"px"
+document.body.style.backgroundPositionY = BackgroundPos+"px"
+setInterval(() => {BackgroundPos+=1920},BackgroundAnimationSpeed)
+setInterval(() => {document.body.style.backgroundPositionY = BackgroundPos+"px" },BackgroundAnimationSpeed)
+setInterval(() => {document.body.style.backgroundPositionX = BackgroundPos+"px" },BackgroundAnimationSpeed)
